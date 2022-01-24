5ffd0b87f0ac34d7265a779ce6a18fee250edc78
Aydın DUYGU <aydinduyku78@gmail.com>
2022-01-24 18:01:46 +0300

    Initial commit

---
+++ src/Response.java
@@ -0,0 +1,35 @@
+import java.io.Serializable;
+
+public class Response implements Serializable {
+
+    private long id;
+    private long requestId;
+    private int responseType;
+    private String responseMessage;
+
+    public Response(){}
+
+    public long getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(long requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public int getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(int responseType) {
+        this.responseType = responseType;
+    }
+}
